ed68d13463b2a7838a9f8a527165f4943f1fcfe6
MilanCDRS <milan.cdrs@gmail.com>
2025-01-30 16:21:10 +0100

    #7 set up SAVE classes

---
+++ EasySave/EasySave/FullSave.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave
+{
+    internal class FullSave : SaveJob
+    {
+        //ATTRIBUTES
+
+
+
+        //CONTRUCTOR
+        public FullSave(string name, string sourcePath, string targetPath) : base(name, sourcePath, targetPath)
+        {
+        }
+
+        //METHODS
+        public override bool CreateSave(string name, string sourcePath, string targetPath)
+        {
+            return true;
+        }
+
+        public override bool UpdateSave(string name)
+        {
+            return true;
+        }
+
+        public override bool RestoreSave(string name)
+        {
+            return true;
+        }
+
+        public override bool DeleteSave(string name)
+        {
+            return true;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+
+            return str;
+        }
+    }
+}
---
+++ EasySave/EasySave/SaveJob.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave
+{
+    public abstract class SaveJob
+    {
+        // ATTRIBUTES
+        public string Name { get; set; }
+        public string SourcePath { get; set; }
+        public string TargetPath { get; set; }
+        public DateTime CreationDate { get; set; }
+        
+        //CONSTRUCTOR
+        protected SaveJob(string name, string sourcePath, string targetPath)
+        {
+            Name = name;
+            SourcePath = sourcePath;
+            TargetPath = targetPath;
+            CreationDate = DateTime.Now;
+        }
+
+
+        //METHODS
+        public abstract bool CreateSave(string name, string sourcePath, string targetPath);
+
+        public abstract bool UpdateSave(string name);
+
+        public abstract bool RestoreSave(string name);
+
+        public abstract bool DeleteSave(string name);
+
+        public abstract override string ToString();
+    }
+}
