28facae94427572375b49983ab874f9b510f721a
Guillaume-Finotto <150013338+Guillaume-Finotto@users.noreply.github.com>
2025-02-03 13:37:14 +0100

    #9 Logger Json Fonctionnel (#14)

--- EasySave/EasySave/Program.cs
+++ EasySave/EasySave/Program.cs
@@ -1,6 +1,4 @@
-﻿using EasySave;
+using EasySave;
 
-Console.WriteLine("Hello, World!");
-FullSave save = new FullSave("TEST1", "C:\\Users\\Milan\\Desktop\\projetCESI\\Tests\\Tests1", "C:\\Users\\Milan\\Desktop\\projetCESI\\Saves\\");
-
-save.CreateSave();
+Logger.Initialize();
+Logger.GetInstance().Log(new FullSave("guigui", "LoLo", "Tom"));
