3c4574be888572b17ab1fc68ff036d942b924ae5
MilanCDRS <milan.cdrs@gmail.com>
2025-02-10 16:04:18 +0100

    add CryptoSoft

---
+++ EasySave/CryptoSoft/CryptoSoft.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Net.Security;
+using System.Security.Cryptography;
+
+namespace CryptoSoftLib
+{
+    public static class CryptoSoft
+    {
+        public static string exePath = "CryptoSoft/CryptoSoft.exe";
+        public static void EncryptDecrypt(string filePath, string key)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = exePath,
+                Arguments = $"\"{filePath}\" \"{key}\"", // Passer les arguments en les entourant de guillemets
+                RedirectStandardOutput = true, // Pour récupérer la sortie si nécessaire
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+
+            try
+            {
+               Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur lors de l'exécution : " + ex.Message);
+            }
+        }
+    }
+}
