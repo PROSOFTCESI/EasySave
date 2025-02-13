d1bb515192ee94f22aaff10e8d76757799f421b8
CDRS <68082649+MilanCDRS@users.noreply.github.com>
2025-02-13 10:12:26 +0100

    Update CryptoSoft.cs

--- EasySave/EasySave/Utils/CryptoSoft.cs
+++ EasySave/EasySave/Utils/CryptoSoft.cs
@@ -1,87 +1,95 @@
+using System.Diagnostics;
+using System.Net.Security;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+using EasySave.Utils;
 using EasySave.Utils.JobStates;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CryptoSoftLib;
-using System.Text.Json;
-using System.Net.Http.Json;
-using System.IO.Enumeration;
-using Newtonsoft.Json;
-using JsonSerializer = System.Text.Json.JsonSerializer;
-using static System.Net.Mime.MediaTypeNames;
+using Newtonsoft.Json.Linq;
 
-namespace EasySave.Utils
+namespace CryptoSoftLib
 {
-    public class SettingsJson
+    public static class CryptoSoft
     {
-        private static readonly string FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "EasySave");
-        private static readonly string FilePath = Path.Combine(FolderPath, "settings.json");
+        private static string currentDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
 
-        private static SettingsJson? instance;
-
-        public static SettingsJson GetInstance()
+        private static string exePath = Path.Combine(currentDir, "CryptoSoft/CryptoSoft.exe");   
+        
+        private static string Key()
         {
-            instance ??= new SettingsJson();
-            return instance;
-        }
-      
-        public SettingsJsonDefinition GetContent()
-        {
-            Initialize();
-            SettingsJsonDefinition content = JsonSerializer.Deserialize<SettingsJsonDefinition>(File.ReadAllText(FilePath));
-            return content;
+            return SettingsJson.GetInstance().GetContent().EncryptionKey;
         }
 
-        public void Initialize()
+        public static string GenerateKey()
+        {              
+            return Guid.NewGuid().ToString();
+        }
+        public static void EncryptDecryptFile(string filePath, string key = null)
         {
-            if (!Directory.Exists(FolderPath))
+            if(key is null){
+                key = Key();
+            }
+
+            if (ExtentionToEncrypt()[0] != "*")
             {
-                Directory.CreateDirectory(FolderPath);
+                if (!ExtentionToEncrypt().Contains(new FileInfo(filePath).Extension))
+                {
+                    Console.WriteLine("non");
+                    return;
+                }
             }
 
-            if (!File.Exists(FilePath))
+            ProcessStartInfo psi = new ProcessStartInfo
             {
-                File.WriteAllText(FilePath, "[]");                
+                FileName = exePath,
+                Arguments = $"\"{filePath}\" \"{key}\"", // Passer les arguments en les entourant de guillemets
+                RedirectStandardOutput = true, // Pour récupérer la sortie si nécessaire
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+
+            try
+            {
+                Process.Start(psi);
             }
-            InitContent();
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur lors de l'exécution : " + ex.Message);
+            }
         }
 
-        private void InitContent()
+        public static void EncryptDecryptFolder(string folder, string key = null)
         {
-            SettingsJsonDefinition newContent = new SettingsJsonDefinition();
-              
-            try{
-                newContent = JsonSerializer.Deserialize<SettingsJsonDefinition>(File.ReadAllText(FilePath));
+            if(key is null)
+            {
+                key = Key();
             }
-            catch { }
-                     
 
-            newContent.Name = newContent.Name != null ? newContent.Name : "EasySave";
-            newContent.EncryptionKey = newContent.EncryptionKey != null ? newContent.EncryptionKey : CryptoSoft.GenerateKey();
-            newContent.extensionsToEncrypt = newContent.extensionsToEncrypt != null ? newContent.extensionsToEncrypt : "*";
-            newContent.selectedCulture = newContent.selectedCulture != null ? newContent.selectedCulture : "FR";
-            newContent.logFormat = newContent.logFormat != null ? newContent.logFormat : "json";
+            // Get information about the source directory
+            var dir = new DirectoryInfo(folder);
 
-            string json = JsonConvert.SerializeObject(newContent, Formatting.Indented);
-            File.WriteAllText(FilePath, json);
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+            // Cache directories before we start copying
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (FileInfo file in dir.GetFiles())
+            {                
+                CryptoSoft.EncryptDecryptFile(file.FullName, key);                
+            }
+
+            // RECURSIVITY : Copy the files from the sub directories
+            foreach (DirectoryInfo subDir in dirs)
+            {
+                EncryptDecryptFolder(subDir.FullName, key);
+            }
         }
 
-        public void Update(SettingsJsonDefinition newContent)
+        public static string[] ExtentionToEncrypt()
         {
-            string json = JsonConvert.SerializeObject(newContent, Formatting.Indented);
-            File.WriteAllText(FilePath, json);
-        } 
-    }
-
-    public class SettingsJsonDefinition
-    {
-        public string Name { get; set; }
-        public string EncryptionKey { get; set; }
-        public string selectedCulture { get; set; }
-        public string extensionsToEncrypt { get; set; }
-        public string logFormat { get; set; }
+           return SettingsJson.GetInstance().GetContent().extensionsToEncrypt.Split(' ');
+        }
     }
 }
-
