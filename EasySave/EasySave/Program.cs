22ce7f31140513b4c3c3ac0ef95b25df492e9fd5
Sonixray <loangayrard@gmail.com>
2025-02-22 14:17:45 +0100

    #71 : Fix console app

--- EasySave/EasySave/Program.cs
+++ EasySave/EasySave/Program.cs
@@ -10,11 +10,6 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 Console.WriteLine("Hello, World!");
 
-FullSave save = new FullSave("TEST", "C:\\Users\\Milan\\Desktop\\projetCESI\\Tests\\TestDiff", "C:\\Users\\Milan\\Desktop\\projetCESI\\Saves\\NewSave");
-
-save.CreateSave();
-//save.Save();
-
-//FileStructureJson.GetAdvancement("C:\\Users\\Milan\\Desktop\\projetCESI\\Saves\\NewSave\\FullSave_20_02_2025-13_32_47\\.fileStructure.json");
-
-//Directory.CreateDirectory("C:\\Users\\Milan\\Desktop\\projetCESI\\Saves\\NewSave\\Test\\test")*/
+SettingsJson.GetInstance().Initialize();
+Logger.GetInstance().Initialize("EasySave", Logger.LogExportType.json);
+new ConsoleManager().Launch();
